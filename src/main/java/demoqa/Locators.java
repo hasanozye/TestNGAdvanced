8164dd3e9b1753537139b582a201d069234121aa
Hasan Ozyer <hasanozyer06@gmail.com>
2023-08-23 21:31:24 +0300

    optimized locators

--- src/main/java/demoqa/Locators.java
+++ src/main/java/demoqa/Locators.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.By;
 
 public interface Locators {
     String url = "https://demoqa.com/webtables";
-    By lFirstDelete = By.xpath("//span[@id='delete-record-1']");
-    By lSecondDelete = By.xpath("//span[@id='delete-record-2']");
-    By lThirdDelete = By.xpath("//span[@id='delete-record-3']");
+    By lDelButtons = By.xpath("//span[starts-with(@id,'delete')]");
     By lAddButton = By.xpath("//button[@id='addNewRecordButton']");
     By lFirstName = By.cssSelector("#firstName");
     By lLastName = By.cssSelector("#lastName");
@@ -15,4 +13,5 @@ public interface Locators {
     By lSalary = By.cssSelector("#salary");
     By lDepartment = By.cssSelector("#department");
     By lSubmitButton = By.id("submit");
+    By lSortByFirstName = By.xpath("//div[text()='First Name']");
 }
