79be1b34fe0f754b9ab856a9878da9e0fc32e6a5
Hasan Ozyer <hasanozyer06@gmail.com>
2023-08-23 21:29:42 +0300

    delete the table data is optimized with while loop

--- src/main/java/demoqa/DemoHomePage.java
+++ src/main/java/demoqa/DemoHomePage.java
@@ -3,6 +3,7 @@ package demoqa;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -22,12 +23,20 @@ public class DemoHomePage extends BaseClass {
 
     public void login() {
         driver.get(url);
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(lDelButtons, 1));
     }
 
     public void deleteTheTableDatas() {
-        click(lFirstDelete);
-        click(lSecondDelete);
-        click(lThirdDelete);
+
+        List<WebElement> dels = driver.findElements(lDelButtons);
+        while (dels.size() > 0) {
+            int initialSize = dels.size();
+            click(dels.get(0));
+            wait.until(ExpectedConditions.numberOfElementsToBeLessThan(lDelButtons, initialSize));
+            dels = driver.findElements(lDelButtons);
+        }
+
+
     }
 
 }
